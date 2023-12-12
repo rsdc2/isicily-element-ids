706b6bc72c8f1c10604b316d49f13ed9ad6e8570
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-12-12 15:37:34 +0000

    moved event handlers to eventHandlers.js

---
+++ src/eventHandlers.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @param {MouseEvent} ev 
+ */
+
+const handleCompress = (ev) => {
+    document.getElementById("result").textContent = decToBase(idToConvert(), BASE100)
+}
+
+/**
+ * 
+ * @param {MouseEvent} ev 
+ */
+const handleDecompress = (ev) => {
+    document.getElementById("result").textContent = insertISic(String(baseToDec(inputIdElem1().value, BASE100)))
+}
+
+/**
+ * 
+ * @param {MouseEvent} ev 
+ */
+const handleMidPoint = (ev) => {
+    document.getElementById("result").textContent = midPointBetweenValues(inputIdElem1().value, inputIdElem2().value, BASE100)
+}
--- src/index.js
+++ src/index.js
@@ -1,33 +1,9 @@
-    
+
 /**
  * @returns {bigint}
  */
 const idToConvert = () => BigInt(inputIdElem1().value)
 
-/**
- * 
- * @param {MouseEvent} ev 
- */
-
-const handleCompress = (ev) => {
-    document.getElementById("result").textContent = decToBase(idToConvert(), BASE100)
-}
-
-/**
- * 
- * @param {MouseEvent} ev 
- */
-const handleDecompress = (ev) => {
-    document.getElementById("result").textContent = insertISic(String(baseToDec(inputIdElem1().value, BASE100)))
-}
-
-/**
- * 
- * @param {MouseEvent} ev 
- */
-const handleMidPoint = (ev) => {
-    document.getElementById("result").textContent = midPointBetweenValues(inputIdElem1().value, inputIdElem2().value, BASE100)
-}
 
 function addListeners() {
     compressBtn().addEventListener("click", handleCompress)

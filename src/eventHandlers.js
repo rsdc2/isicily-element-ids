e2a88af3f0c913c1d85be7422e6a4ca4a0d33b15
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-12-12 15:54:56 +0000

    formatting

--- src/eventHandlers.js
+++ src/eventHandlers.js
@@ -4,7 +4,8 @@
  */
 
 const handleCompress = (ev) => {
-    document.getElementById("result").textContent = decToBase(idToConvert(), BASE100)
+    document.getElementById("result").textContent = 
+        decToBase(idToConvert(), BASE100)
 }
 
 /**
@@ -12,7 +13,8 @@ const handleCompress = (ev) => {
  * @param {MouseEvent} ev 
  */
 const handleDecompress = (ev) => {
-    document.getElementById("result").textContent = insertISic(String(baseToDec(inputIdElem1().value, BASE100)))
+    document.getElementById("result").textContent = 
+        insertISic(String(baseToDec(inputIdElem1().value, BASE100)))
 }
 
 /**
@@ -20,5 +22,6 @@ const handleDecompress = (ev) => {
  * @param {MouseEvent} ev 
  */
 const handleMidPoint = (ev) => {
-    document.getElementById("result").textContent = midPointBetweenValues(inputIdElem1().value, inputIdElem2().value, BASE100)
+    document.getElementById("result").textContent = 
+        midPointBetweenValues(inputIdElem1().value, inputIdElem2().value, BASE100)
 }

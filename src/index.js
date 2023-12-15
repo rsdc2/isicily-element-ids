980db9d3cfbf525c7ec2325f2a20453be39422dc
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2023-12-15 10:56:36 +0000

    add click even handler

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ function addListeners() {
     flipBtn.addEventListener("click", handleFlip)
     textInput1.addEventListener("input", handleChangeEvent)
     textInput2.addEventListener("input", handleChangeEvent)
+    textInput1.addEventListener("click", handleClickEvent)
+    textInput2.addEventListener("click", handleClickEvent)
+
 }
 
 addListeners()

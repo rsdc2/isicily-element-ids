4b414020e64a2e456da09bc52ada6566e85cf590
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-01-18 16:10:28 +0000

    reordering of methods

--- src/Pure/format.js
+++ src/Pure/format.js
@@ -4,24 +4,6 @@ import Bases from "./bases.js"
 export default class Format {
 
     /**
-     * Underline Greek characters
-     * @param {string} s 
-     * @returns {string} an HTML string with Greek characters surrounded by <u> tags
-     */
-    static underlineGreek = (s) => {
-        return s.replace(/([α-ωΑ-Ω]+)/g, "<u>$1</u>")
-    }
-
-    /**
-     * Removes underlining from Greek characters
-     * @param {string} s 
-     * @returns 
-     */
-    static removeUnderline (s) {
-        return s.replace(/\<\/?u\>/g, "")
-    }
-
-    /**
      * Inserts string elements into a number corresponding to an
      * ISicily ID + 5 digit element id
      * @param {string} s 
@@ -50,6 +32,24 @@ export default class Format {
      */
     static removeISic = (s) => {
         return s.replace("ISic", "").replace("-", "")
+    }
+
+    /**
+     * Removes underlining from Greek characters
+     * @param {string} s 
+     * @returns 
+     */
+    static removeUnderline (s) {
+        return s.replace(/\<\/?u\>/g, "")
+    }
+
+    /**
+     * Underline Greek characters
+     * @param {string} s 
+     * @returns {string} an HTML string with Greek characters surrounded by <u> tags
+     */
+    static underlineGreek = (s) => {
+        return s.replace(/([α-ωΑ-Ω]+)/g, "<u>$1</u>")
     }
 }
 

3482779845fdba2a78c6d1bd51579d8a8187f4e3
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-02-07 13:35:39 +0000

    initial methods

--- src/Pure/epidoc/ab.js
+++ src/Pure/epidoc/ab.js
@@ -1,5 +1,23 @@
 import HasXMLElem from "../xml/hasxmlelem.js";
+import Constants from "../constants.js";
+
 
 export default class Ab extends HasXMLElem {
-    
+    /**
+     * 
+     * @param {Element} elem 
+     */
+    constructor(elem) {
+        super(elem)
+
+        this.assertNameNS("ab", Constants.TEINS)
+    }
+
+    /**
+     * 
+     * @param {Element} elem 
+     */
+    static fromElem(elem) { 
+        return new Ab(elem)
+    }
 }

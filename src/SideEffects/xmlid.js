6ba1c400bed508cc8517d7e6c5ddc61c3c78a7c6
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-02-07 17:16:57 +0000

    change name to xmlid

---
+++ src/SideEffects/xmlid.js
@@ -0,0 +1,97 @@
+import FilePicker from "./filepicker.js"
+import FileDownloader from "./filedownloader.js"
+import {newFileReader} from "./filereader_.js"
+import Convert from "../Pure/convert.js"
+import Base from "../Pure/base.js"
+import Message from "./message.js"
+import { 
+    BaseIndexError, 
+    BaseLengthError, 
+    BaseValueError, 
+    ConversionError, 
+    CSVFormatError, 
+    FileError} from "../Pure/errors.js"
+import Constants from "../Pure/constants.js"
+
+const {BASE100, BASE52} = Constants
+import EpiDoc from "./epidoc/epidoc.js"
+
+
+export default class XMLID {
+    #picker 
+    #reader
+
+    /**
+     * Create a new XMLIDApplier instance that applies IDs
+     * to tokens in a tokenized EpiDoc XML file.
+     * @param {string} filename
+     * @param {"setIDs"|"expandIDs"|"compressIDs"} mode 
+     */
+    constructor(filename, mode) {
+
+        // Initialize reader
+        // cf. https://developer.mozilla.org/en-US/docs/Web/API/FileReader
+        this.#reader = newFileReader(
+            (e) => {
+                const contents = /** @type {string} */ (e.target.result) 
+
+                // String because will use readAsText method to read
+                try {
+                    const xml = new DOMParser().parseFromString(
+                        contents, 
+                        'application/xml'
+                    )
+
+                    const epidoc = EpiDoc.fromDoc(xml)
+
+                    if (mode === "setIDs") {
+                        epidoc.setXMLIDs(Base.fromBaseChars(BASE100))
+                    } else if (mode === "expandIDs") {
+                        epidoc.expandXMLIDs(Base.fromBaseChars(BASE100))
+                    } else if (mode === "compressIDs") {
+                        epidoc.compressXMLIDs(Base.fromBaseChars(BASE100))
+                    }
+                    // console.log(epidoc.editions[0].abs.flatMap(ab => ab.descendants))
+
+                    const xmlStr = new XMLSerializer().serializeToString(xml)
+
+                    const downloader = new FileDownloader(xmlStr)
+                    downloader.download(filename)  
+                } catch (error) {
+                    Message.alert(
+                        `Unknown error: please refer to the developer console`
+                    )
+                    throw error
+                }  
+            }
+        )
+
+        // Initialize file picker
+        this.#picker = new FilePicker ([".xml"], this.#reader)
+        
+        // Show the file picker
+        this.#picker.load()
+    }
+ 
+
+    /**
+     * Create an XMLIDApplier that downloads a file with a specific filename
+     * when called
+     * @param {string} filename 
+     * @param {"setIDs"|"expandIDs"|"compressIDs"} mode 
+     * @returns 
+     */
+    static create(filename, mode) {
+
+        function inner() {
+            try {
+                return new XMLID(filename, mode) 
+            } catch (error) {
+                if (error instanceof FileError) {
+                    Message.alert(error.message)
+                }
+            }
+        }
+        return inner
+    }
+}

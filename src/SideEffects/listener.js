98904942caa15f604f67f0a47d041edc02434be4
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-02-07 17:16:43 +0000

    expand and set xml id buttons

--- src/SideEffects/listener.js
+++ src/SideEffects/listener.js
@@ -1,7 +1,7 @@
 import Handlers from "./handlers.js"
 import Elems from "./elems.js"
 import CSVIDConverter from "./csvIDConverter.js"
-import XMLIDApplier from "./xmlIDApplier.js"
+import XMLIDApplier from "./xmlid.js"
 
 export default class Listener {
     static addListeners() {
@@ -10,11 +10,12 @@ export default class Listener {
         Elems.bodyDiv.addEventListener("click", Handlers.hideAllPopups)
         Elems.compressBtn.addEventListener("click", Handlers.handleToggleMode)
         Elems.convertOldIDsBtn.addEventListener("click", CSVIDConverter.create("id_conversions.csv"))
-        Elems.idXMLBtn.addEventListener("click", XMLIDApplier.create("xml_with_ids.xml"))
+        Elems.expandXMLIDBtn.addEventListener("click", XMLIDApplier.create("xml_with_expanded_ids.xml", "expandIDs"))
         Elems.flipBtn.addEventListener("click", Handlers.handleFlip)
         Elems.midPointBtn.addEventListener("click", Handlers.handleToggleMode)
         Elems.notesBtn.addEventListener("click", Handlers.handleToggleShowNotes)
         Elems.notesDiv.addEventListener("click", Handlers.handleToggleShowNotes)
+        Elems.setXMLIDBtn.addEventListener("click", XMLIDApplier.create("xml_with_ids.xml", "setIDs"))
         Elems.textInput1.addEventListener("keyup", Handlers.handleUpdateInput)
         Elems.textInput2.addEventListener("keyup", Handlers.handleUpdateInput)
         Elems.textInput2.addEventListener("keyup", Handlers.handleUpdateInput)

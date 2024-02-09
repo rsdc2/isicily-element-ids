614eb76e2f5e78fe3bbda17a0b8012ca38382482
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-02-09 14:18:53 +0000

    delegate to textelems

--- src/SideEffects/epidoc/epidoc.js
+++ src/SideEffects/epidoc/epidoc.js
@@ -3,9 +3,7 @@ import HasXMLDoc from "../xml/hasxmldoc.js";
 import Edition from "./elements/edition.js";
 import Base from "../../Pure/base.js";
 import Compress from "../../Pure/compress.js";
-import TextElem from "./textElem.js";
-import { MidpointIDError } from "./errors.js";
-import NullIDBlock from "./nullidblock.js";
+import TextElems from "./textElems.js";
 import NullIDBlocks from "./nullidblocks.js";
 
 
@@ -15,12 +13,10 @@ export default class EpiDoc extends HasXMLDoc {
      * Set the IDs in the TextElements to their compressed
      * form
      * @param {Base} base The Base of the IDs in the document
+     * @returns {TextElems}
      */
     compressXMLIDs(base) {
-        this.textElems.forEach ( (elem) => {
-            elem.compressID(base)
-        })
-        return this.textElems
+        return this.textElems.compressXMLIDs(base)
     }
 
     /**
@@ -28,12 +24,10 @@ export default class EpiDoc extends HasXMLDoc {
      * to their Base100 form
      * @param {Base} oldBase The Base of the IDs in the document
      * @param {Base} newBase
+     * @returns {TextElems}
      */
     convertXMLIDs(oldBase, newBase) {
-        this.textElems.forEach ( (elem) => {
-            elem.convertID(oldBase, newBase)
-        })
-        return this.textElems
+        return this.textElems.convertXMLIDs(oldBase, newBase)
     }
 
     get editions() {
@@ -46,12 +40,10 @@ export default class EpiDoc extends HasXMLDoc {
      * Set the IDs in the TextElements to their expanded
      * (decompressed) form
      * @param {Base} base The Base of the IDs in the document
+     * @returns {TextElems}
      */
     expandXMLIDs(base) {
-        this.textElems.forEach ( (elem) => {
-            elem.expandID(base)
-        })
-        return this.textElems
+        return this.textElems.expandXMLIDs(base)
     }
 
     /**
@@ -59,15 +51,16 @@ export default class EpiDoc extends HasXMLDoc {
      * \@xml:id and assigns an \@xml:id between those that 
      * have already been assigned
      * @param {Base} base 
+     * @returns {TextElems}
      */
     setMidpointXMLIDs(base) {
-        const blocks = NullIDBlocks.fromTextElems(this.textElems, base)
-        return blocks.assignIDs()
+        return this.textElems.setMidpointXMLIDs(base)
     }
 
     /**
      * Create a new EpiDoc object from an XMLDocument
      * @param {XMLDocument} doc 
+     * @returns {EpiDoc}
      */
     static fromDoc(doc) {
         return new EpiDoc(doc)
@@ -86,15 +79,17 @@ export default class EpiDoc extends HasXMLDoc {
     /**
      * Assign \@xml:id to each descendant text element in place
      * @param {Base} base
+     * @returns {TextElems}
      */
     setXMLIDs(base) {
-        this.textElems.forEach( (elem, index) => {
+        this.textElems.elems.forEach( (elem, index) => {
             const tokenDecimalID = BigInt((index + 1) * 10).toString()
             const paddedTokenDecimalID = tokenDecimalID.padStart(5, "0")
             const fullID = this.id.concat("-", paddedTokenDecimalID)
             const compressed = Compress.compressID(base)(fullID)
             elem.setXMLID(compressed) 
         })
+        return this.textElems
     }
 
     get textElems() {
@@ -102,7 +97,7 @@ export default class EpiDoc extends HasXMLDoc {
             edition => edition.descendantTextElems
         )
 
-        return elems
+        return new TextElems(elems)
     }
 
 }

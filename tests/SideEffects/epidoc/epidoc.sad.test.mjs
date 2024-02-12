e5960305946c9cf60468893f82fa460dc2df7497
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-02-12 12:15:31 +0000

    better test names

--- tests/SideEffects/epidoc/epidoc.sad.test.mjs
+++ tests/SideEffects/epidoc/epidoc.sad.test.mjs
@@ -8,7 +8,7 @@ import { ISicElementIDError } from "../../../src/Pure/errors.js"
 
 const epidocFp = "./tests/SideEffects/epidoc/files/ISic000001_invalid.xml"
 
-test("Load and read EpiDoc file with error", (t) => {
+test("Load and read invalid EpiDoc file with error", (t) => {
     assert.throws(() => {
         const xmlStr = readFileSync(epidocFp, {encoding: "utf8"})
         const xml = new JSDOM(xmlStr, {contentType: "application/xml"}).window.document

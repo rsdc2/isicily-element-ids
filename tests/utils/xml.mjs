cfb7f82afb2ab9ac54b9dfea8f623b4b0cfa6066
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-02-12 17:48:33 +0000

    another helpful link

--- tests/utils/xml.mjs
+++ tests/utils/xml.mjs
@@ -2,6 +2,7 @@ import { JSDOM } from "jsdom"
 
 // Cf. https://github.com/jsdom/jsdom/blob/main/lib/jsdom/living/domparsing/DOMParser-impl.js
 // cf. https://stackoverflow.com/questions/59668971/save-xml-parsed-by-jsdom-as-a-file
+// cf. https://stackoverflow.com/questions/42649700/using-domparser-in-javascript-testing-with-mocha-and-jsdom
 
 export const DOMParser = new JSDOM().window.DOMParser
 

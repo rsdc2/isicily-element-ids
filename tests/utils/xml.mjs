5eab3179c4cecd69f56fa1e73e07d617ab94226b
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-02-12 17:45:05 +0000

    add XMLSerializer

--- tests/utils/xml.mjs
+++ tests/utils/xml.mjs
@@ -1,4 +1,8 @@
 import { JSDOM } from "jsdom"
 
+// Cf. https://github.com/jsdom/jsdom/blob/main/lib/jsdom/living/domparsing/DOMParser-impl.js
+// cf. https://stackoverflow.com/questions/59668971/save-xml-parsed-by-jsdom-as-a-file
+
 export const DOMParser = new JSDOM().window.DOMParser
 
+export const XMLSerializer = new JSDOM().window.XMLSerializer

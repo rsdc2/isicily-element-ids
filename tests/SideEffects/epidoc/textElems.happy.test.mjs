6b5caf21185b0d0258814b42d0d848c442beb0b8
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-02-12 16:58:53 +0000

    use Map instead of Object; test ids align

--- tests/SideEffects/epidoc/textElems.happy.test.mjs
+++ tests/SideEffects/epidoc/textElems.happy.test.mjs
@@ -1,24 +1,26 @@
-import { JSDOM } from "jsdom"
 import { test } from "node:test"
 import assert from "node:assert/strict"
-import { readFileSync } from "node:fs"
+import { loadEpiDoc, writeXML } from "../../utils/file.mjs"
 
-import EpiDoc from "../../../src/SideEffects/epidoc/epidoc.js"
-import { ISicElementIDError } from "../../../src/Pure/errors.js"
+import { Arr } from "../../../src/Pure/arr.js"
 import { NullIDError } from "../../../src/SideEffects/epidoc/errors.js"
 import Base from "../../../src/Pure/base.js"
 import Constants from "../../../src/Pure/constants/constants.js"
+import { Config } from "../../../src/config.js"
 
 const {BASE100, BASE52} = Constants
 const base100 = Base.fromBaseChars(BASE100)
+const relpath = "./tests/SideEffects/epidoc/files/"
 
-const epidocFp = "./tests/SideEffects/epidoc/files/ISic000001_valid.xml"
+const files = new Map([
+    ["ISic000001Valid", relpath + "ISic000001_valid.xml"],
+    ["ISic001174NoIDs", relpath + "ISic001174_tokenized_without_ids.xml"],
+    ["ISic001174WithIDs", relpath + "ISic001174_tokenized_with_ids_pyepidoc.xml"]
+])
 
 
 test("Put an @xml:id on all text elements", (t) => {
-    const xmlStr = readFileSync(epidocFp, { encoding: "utf8" })
-    const xml = new JSDOM(xmlStr, { contentType: "application/xml" }).window.document
-    const epidoc = new EpiDoc(xml)
+    const epidoc = loadEpiDoc(files.get("ISic000001Valid"))
     
     assert.doesNotThrow(() => {
         epidoc.textElems.assertNoIDs()
@@ -33,9 +35,7 @@ test("Put an @xml:id on all text elements", (t) => {
 
 
 test("Put an @xml:id on element subset", (t) => {
-    const xmlStr = readFileSync(epidocFp, { encoding: "utf8" })
-    const xml = new JSDOM(xmlStr, { contentType: "application/xml" }).window.document
-    const epidoc = new EpiDoc(xml)
+    const epidoc = loadEpiDoc(files.get("ISic000001Valid"))
     const elems = epidoc.textElems
     
     assert.doesNotThrow(() => {
@@ -48,4 +48,23 @@ test("Put an @xml:id on element subset", (t) => {
     const ws = elems.subset(["w"])
     assert.strictEqual(lbs[0].xmlid, null)
     assert.notStrictEqual(ws[0].xmlid, null)
+})
+
+
+test("Check IDs are the same as those assigned by PyEpiDoc", (t) => {
+    const withIDs = loadEpiDoc(files.get("ISic001174WithIDs"))
+    const noIDs = loadEpiDoc(files.get("ISic001174NoIDs"))
+    
+    noIDs.textElems.setXMLIDs(base100, noIDs.id, Config.elementsForXMLID)
+    writeXML(noIDs.doc, relpath + "ISic001174_tokenized_with_ids_js.xml")
+
+    const idElems = withIDs.textElems.elems
+    const noIDElems = noIDs.textElems.elems
+
+    assert.strictEqual(idElems.length, noIDElems.length)
+
+    Arr.zip(idElems, noIDElems).forEach(([pyepidocID, jsID]) => {
+        assert.strictEqual(pyepidocID.xmlid, jsID.xmlid)
+    });
+
 })

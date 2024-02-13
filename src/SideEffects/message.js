03eddec3314b49b26e3ebcfe30985c5888d350bf
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-02-13 15:27:05 +0000

    create new category of alert: error and alert

--- src/SideEffects/message.js
+++ src/SideEffects/message.js
@@ -1,14 +1,27 @@
 import Attrs from "./attrs.js"
 import Elems from "./elems.js"
 
+const { messageDiv } = Elems
+
 export default class Message {
+    /**
+     * 
+     * @param {string} s 
+     */
+    static alert = s => {
+        messageDiv.classList.remove("error")
+        messageDiv.textContent = s
+        Message.show()
+    }
+
     /**
      * Broadcasts a message to the user via the top banner
      * @param {string} s  
      * @returns {void}
      */
-    static alert = s => {
-        Elems.messageDiv.textContent = s
+    static error = s => {
+        messageDiv.classList.add("error")
+        messageDiv.textContent = s
         Message.show()
     }
 
@@ -16,14 +29,14 @@ export default class Message {
      * Hide the message bar
      */
     static hide = () => {
-        Attrs.addClasses(Elems.messageDiv)("hidden")
+        Attrs.addClasses(messageDiv)("hidden")
     }
 
     /**
      * Show the message bar
      */
     static show = () => {
-        Attrs.removeClasses(Elems.messageDiv)("hidden")
+        Attrs.removeClasses(messageDiv)("hidden")
     }
 }
 

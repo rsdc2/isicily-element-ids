696dff1a49e8bb9b9c640bada4de590118030165
Robert Crellin <54702776+rsdc2@users.noreply.github.com>
2024-02-19 17:27:03 +0000

    same length and completely different

---
+++ src/Pure/stringedit.js
@@ -0,0 +1,53 @@
+
+import { Arr } from "./arr.js"
+
+/**
+ * 
+ * @param {string} s1
+ * @param {string} s2
+ * @returns {number} 
+ */
+function editDistance(s1, s2) {
+
+    /**
+     * 
+     * @param {string} s1 
+     * @param {string} s2
+     * @return {number} 
+     */
+    function sameLength(s1, s2) {
+        const zipped = Arr.zip(Array.from(s1), Array.from(s2));
+
+        let sum = 0;
+
+        zipped.forEach(([char1, char2]) => {
+            if (char1 !== char2) {
+                sum = sum + 1
+            }
+        });
+
+        return sum;
+
+    }
+
+    if (s1 === s2) {
+        return 0
+    }
+
+    if (s1.length === 0 && s2.length !== 0) {
+        return s2.length
+    }
+
+    if (s1.length !== 0 && s2.length === 0) {
+        return s1.length
+    }
+
+    if (s1.length === s2.length) {
+        return sameLength(s1, s2)        
+    }
+
+    const lengthDiff = s1.length - s2.length
+
+    
+
+}
